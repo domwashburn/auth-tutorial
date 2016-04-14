9e695bd440aa46a463b93af6e7a7eb4b2d29559c
Dominick Washburn <domwashburn@us.ibm.com>
2016-04-14 10:54:47 -0500

    added the ability to create a user record and encrypt the users password with bcrypt-node

---
+++ server/controllers/authentication.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	if ( !email || !password ) {
+		return res.status(422).send({ error: 'Email and password must be provided'});
+	}
+
+	// see if a user with the given email exists
+	User.findOne({ email: email }, function(err, existingUser){
+		if (err) { return next(err) };
+		// If a user with that email does exist, return an error.
+		if (existingUser) {
+			return res.status(422).send({ error: 'email is already in use' });
+		}
+		// If the email has not been used before, create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		user.save(function(err) {
+			if (err) { return next(err); }
+
+			// Resopond to the request indicating the user was created
+			res.json({ success: true });
+		});
+	});
+};
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
+});
+
+// on save hook, encrypt the password
+// before the model gets saved, run this (.pre)
+userSchema.pre('save', function(next) {
+	// get access to the instance of the user model
+	const user = this;
+
+	// generate a salt then run the callback function
+	bcrypt.genSalt(10, function(err, salt) {
+		if (err) { return next(err); }
+
+		// hash (encrypt) our password using the salt
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if (err) { return next(err); }
+
+			// overwrite plain text password with the encrypted password
+			user.password = hash;
+			next();
+		})
+	})
 });
 
 // create the model class
