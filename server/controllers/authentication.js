56726f08fb81bc9716bdce510c87b1f075d5c4cb
Dominick Washburn <domwashburn@us.ibm.com>
2016-04-15 11:27:42 -0500

    added a local strategy for authenticating users, created a middleware to verify that a valid username was input before allowing access to a protected route. This project is ready to be an Auth boiler plate :)

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
 	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function (req, res, next) {
+	// User has already had their email and password authenticated
+	// Now we need to give them a token
+	res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
@@ -32,7 +38,7 @@ exports.signup = function(req, res, next) {
 			if (err) { return next(err); }
 
 			// Resopond to the request indicating the user was created
-			res.json({ token: tokenForUser(user) });
+			res.json({ token: tokenForUser(user)});
 		});
 	});
 };
--- server/router.js
+++ server/router.js
@@ -1,5 +1,12 @@
-const Authentication = require('./controllers/authentication')
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// session: false prevents passport from creating a cookie based session
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
+	app.post('/signin', requireSignin, Authentication.signin)
 	app.post('/signup', Authentication.signup);
 } 
